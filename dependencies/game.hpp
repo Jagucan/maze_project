ef13431449ef1135278b8b672746962fc2eb7b57
Jagucan <jjgc.2499@gmail.com>
2023-06-13 02:12:12 -0500

    add comment

--- dependencies/game.hpp
+++ dependencies/game.hpp
@@ -1,6 +1,7 @@
 #ifndef _GAME_HPP_
 #define _GAME_HPP_
 
+
 #include "main.hpp"
 
 
@@ -9,11 +10,11 @@ class Game {
 private:
 
 	float _sax, _say, _fs, slice, _d_del, _w_del, far, xfar, yfar, cos,
-        sin, cut, xdel, ydel, steps, angle, sprite, x, y, sfar, sdir;
+		sin, cut, xdel, ydel, steps, angle, sprite, x, y, sfar, sdir;
 
 	int xobj, yobj, xdir, ydir, col, wallNumL, wallNumT, obj, xx, yy, xy, yx,
-        cheight, dop, ssize, shor, sver, mapFloor, ceil_tile, pitch = 0, 
-        width, height, mapWidth, mapHeight, mapWallW,
+		cheight, dop, ssize, shor, sver, mapFloor, ceil_tile, pitch = 0,
+		width, height, mapWidth, mapHeight, mapWallW,
 		mapWallH, mapWallWw, mapWallWh, mapObjsW, mapObjsH,
 		mapObjsWw, mapObjsWh, colorR, colorG, colorB,
 		renderDataSize = 0, objMapSize, renderMax, halfHeight;
@@ -22,11 +23,11 @@ private:
 
 	Uint32 colorConv, ceilColor;
 
-    SDL_Renderer *renderer;
+	SDL_Renderer *renderer;
 
 	SDL_Surface *wallSurf, *cielSurf, *objSurf;
 
-    SDL_Texture *textures, *walls, *Objs;
+	SDL_Texture *textures, *walls, *Objs;
 
 	Uint32 *pixBuffer = nullptr;
 
@@ -42,57 +43,111 @@ private:
 
 	float camX, camY, camA, camB, bright;
 
+
 public:
 
+	/**
+	 * setRenderer - Set the game renderer and configure its dimensions.
+	 * @param sRenderer: Reference to the SDL_Renderer pointer to be assigned.
+	 * @param sWidth: The width of the game renderer.
+	 * @param sHeight: The height of the game renderer.
+	*/
 	void setRenderer(SDL_Renderer*& sRenderer, int sWidth, int sHeight) {
 		renderer = sRenderer;
 		width = sWidth;
 		height = sHeight;
 		halfHeight = height / 2;
-	}
+	};
 
-	void setMap(int* sMap, int sMapWidth, int sMapHeight) {
+	/**
+	 * setMap - Configure the game map and set its dimensions.
+	 * @param sMap: Pointer to the map data to be assigned to the Game class.
+	 * @param sMapWidth: Width of the game map.
+	 * @param sMapHeight: Height of the game map.
+	*/
+	void setMap(int *sMap, int sMapWidth, int sMapHeight) {
 		map = sMap;
 		mapWidth = sMapWidth;
 		mapHeight = sMapHeight;
-	}
+	};
 
+	/**
+	 * setMapObjs - Set the object map data and its size.
+	 * @param sObjMap: Two-dimensional array containing the object map data.
+	 *                  Each row represents an object and contains three elements: x, y, z coordinates.
+	 * @param size: The number of objects in the object map.
+	*/
 	void setMapObjs(float sObjMap[][3], int size) {
 		renderDataSize += size;
 		objMapSize = size;
 		objMap = new float* [size];
 		for (int i = 0; i < size; i++)
 			objMap[i] = sObjMap[i];
-	}
+	};
 
-	void setFlats(int* sFlatFloor, int* sFlatCiel) {
+	/**
+	 * setFlats - Set the flat floor and ceil data.
+	 * @param sFlatFloor: Array containing the flat floor data.
+	 * @param sFlatCiel: Array containing the flat ceil data.
+	*/
+	void setFlats(int *sFlatFloor, int *sFlatCiel) {
 		flatFloor = sFlatFloor;
 		flatCiel = sFlatCiel;
-	}
+	};
 
-	void setLights(float* sLight) {
+	/**
+	 * setLights - Set the light data for the map.
+	 * @param sLight: Array containing the light data for the map.
+	*/
+	void setLights(float *sLight) {
 		mapLight = sLight;
-	}
+	};
 
-	void setCiel(const char* sCiel) {
+	/**
+	 * setCiel - Set the skybox texture for the map.
+	 * @param sCiel: Path to the skybox texture file.
+	*/
+	void setCiel(const char *sCiel) {
 		mapCiel = sCiel;
-	}
+	};
 
-	void setMapWalls(const char* sMapWalls, int sMapWallW, int sMapWallH) {
+	/**
+	 * setMapWalls - Set the wall texture map for the game.
+	 * @param sMapWalls: Path to the wall texture map file.
+	 * @param sMapWallW: Width of the wall texture map.
+	 * @param sMapWallH: Height of the wall texture map.
+	*/
+	void setMapWalls(const char *sMapWalls, int sMapWallW, int sMapWallH) {
 		mapWalls = sMapWalls;
 		mapWallW = sMapWallW;
 		mapWallH = sMapWallH;
-	}
-	
-	void setMapObjs(const char* sMapObjs, int sMapObjsW, int sMapObjsH, int sColorR, int sColorG, int sColorB) {
+	};
+
+	/**
+	 * setMapObjs - Set the game's object map and its properties.
+	 * @param sMapObjs: Path to the object map file.
+	 * @param sMapObjsW: Width of the object map.
+	 * @param sMapObjsH: Height of the object map.
+	 * @param sColorR: Value of the red component of the object color.
+	 * @param sColorG: Value of the green component of the object color.
+	 * @param sColorB: Value of the blue component of the object color.
+	*/
+	void setMapObjs(const char *sMapObjs, int sMapObjsW, int sMapObjsH, int sColorR, int sColorG, int sColorB) {
 		mapObjs = sMapObjs;
 		mapObjsW = sMapObjsW;
 		mapObjsH = sMapObjsH;
 		colorR = sColorR;
 		colorG = sColorG;
 		colorB = sColorB;
-	}
-	
+	};
+
+	/**
+	 * setCam - Set the camera position and orientation.
+	 * @param scamX: X-coordinate of the camera position.
+	 * @param scamY: Y-coordinate of the camera position.
+	 * @param sCamA: Camera's initial angle.
+	 * @param sCamB: Camera's initial pitch angle.
+	*/
 	void setCam(float scamX, float scamY, float sCamA, float sCamB) {
 		camX = scamX;
 		camY = scamY;
@@ -100,8 +155,12 @@ public:
 		camB = sCamB;
 		while (camA > 3.14) camA -= 6.28;
 		while (camA < -3.14) camA += 6.28;
-	}
-	
+	};
+
+	/**
+	 * init - Initialize the game renderer and load necessary resources.
+	 * @param window: SDL_Window pointer to the game window.
+	*/
 	void init(SDL_Window *window) {
 		const SDL_PixelFormat* sFrmt = SDL_GetWindowSurface(window)->format;
 		Uint32 sFrmtG = SDL_GetWindowPixelFormat(window);
@@ -142,7 +201,7 @@ public:
 			RGBobjSurf[i][1] = _fcg;
 			RGBobjSurf[i][2] = _fcb;
 		}
-		
+
 		SDL_FreeSurface(image);
 
 		image = SDL_LoadBMP(mapCiel);
@@ -150,16 +209,20 @@ public:
 		cielSurf = image;
 
 		renderData = new float* [renderDataSize];
-		for (int i = 0; i < renderDataSize; i++)
+		for (int i = 0; i < renderDataSize; i++) {
 			renderData[i] = new float[7];
+		}
 
 		textures = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, width, height);
 		_d_del = 1.f / height;
 		_w_del = 1.f / width;
 
 		colorConv = RGB(colorR, colorG, colorB);
-	}
-	
+	};
+
+	/**
+	 * renderWalls - Render the walls of the game scene using raycasting.
+	 */
 	void renderWalls() {
 		col = 0;
 		for (angle = camA - camB / 2; angle < camA + camB / 2; angle += steps) {
@@ -168,41 +231,51 @@ public:
 			xdir = cos > 0;
 			ydir = sin > 0;
 
-			if (sin) { // y iteration
+			/* y iteration */
+			if (sin) {
 				ydel = camY - (int)camY;
 				yy = 0;
-				if (ydir) // down
+
+				/* down */
+				if (ydir) {
 					while (!yobj) {
 						yy += 1;
 						yfar = (yy - ydel) / sin;
 						yx = camX + yfar * cos;
-						if (yy + camY >= mapHeight || yy + camY < 0 || yx >= mapWidth || yx < 0)
-						{
+						if (yy + camY >= mapHeight || yy + camY < 0 || yx >= mapWidth || yx < 0) {
 							yfar = 100.0;
 							break;
 						}
 						yobj = map[(int)(yy + camY) * mapWidth + yx];
 					}
-				else // up
+				}
+
+				/* up */
+				else {
 					while (!yobj) {
 						yfar = (yy - ydel) / sin;
 						yy -= 1;
 						yx = camX + yfar * cos;
-						if (yy + camY >= mapHeight || yy + camY < 0 || yx >= mapWidth || yx < 0)
-						{
+						if (yy + camY >= mapHeight || yy + camY < 0 || yx >= mapWidth || yx < 0) {
 							yfar = 100.0;
 							break;
 						}
 						yobj = map[(int)(yy + camY) * mapWidth + yx];
 					}
+				}
 			}
-			else
-				yfar = 100.0;
 
-			if (cos) { // x iteration
+			else {
+				yfar = 100.0;
+			}
+				
+			/* x iteration */
+			if (cos) {
 				xdel = camX - (int)camX;
 				xx = 0;
-				if (xdir) // right
+
+				/* right */
+				if (xdir) {
 					while (!xobj) {
 						xx += 1;
 						xfar = (xx - xdel) / cos;
@@ -214,7 +287,10 @@ public:
 						}
 						xobj = map[xy * mapWidth + (int)(xx + camX)];
 					}
-				else // left
+				}
+					
+				/* left */
+				else {
 					while (!xobj) {
 						xfar = (xx - xdel) / cos;
 						xx -= 1;
@@ -226,9 +302,12 @@ public:
 						}
 						xobj = map[xy * mapWidth + (int)(xx + camX)];
 					}
+				}
+					
 			}
-			else
+			else {
 				xfar = 100.0;
+			}
 
 			bright = 1;
 
@@ -267,17 +346,24 @@ public:
 				renderData[col][6] = bright;
 				obj = 0;
 			}
-			else
+
+			else {
 				renderData[col][0] = -1;
+			}
+
 			col++;
 			far = 0;
 			xobj = 0;
 			yobj = 0;
-			if (col >= width)
+			if (col >= width) {
 				break;
+			}
 		}
-	}
-	
+	};
+
+	/**
+	 * renderObjs - Render the objects of the game scene using raycasting.
+	*/
 	void renderObjs() {
 		angle = camA;
 		for (int i = 0; i < objMapSize; i++) {
@@ -288,8 +374,13 @@ public:
 			sfar = sqrtf(powf(camX - x, 2) + powf(camY - y, 2));
 			sdir = atan2f(y - camY, x - camX);
 
-			if (sdir - angle > 3.14) sdir -= 6.28;
-			if (sdir - angle < -3.14) sdir += 6.28;
+			if (sdir - angle > 3.14) {
+				sdir -= 6.28;
+			}
+
+			if (sdir - angle < -3.14) {
+				sdir += 6.28;
+			}
 
 			ssize = fminf(5000, height / sfar);
 			shor = (sdir - angle + camB / 2) / steps - ssize / 2;
@@ -306,84 +397,96 @@ public:
 				renderData[i + width][5] = ssize;
 				renderData[i + width][6] = mapLight[int(x) + int(y) * mapWidth];
 			}
-			else
+
+			else {
 				renderData[i + width][0] = -1;
+			}
 		}
-	}
-	
+	};
+
+	/**
+	 * show - Renders the game scene.
+	 */
 	void show() {
 
 		renderWalls();
 		renderObjs();
 
+		/* Sort the rendering data in descending order based on the y position */
 		for (int i = 0; i < renderDataSize; i++) {
 			renderMax = i;
-			for (int j = i; j < renderDataSize; j++)
+			for (int j = i; j < renderDataSize; j++) {
 				if (renderData[j][1] > renderData[renderMax][1])
 					renderMax = j;
+			}
+
+			/* Swap elements in the renderData array */
 			renderDataMem = renderData[i];
 			renderData[i] = renderData[renderMax];
 			renderData[renderMax] = renderDataMem;
 		}
 
-		// Rendering 
+		/* Rendering */
+		/* Lock the texture to access its pixel data */
 		if (!SDL_LockTexture(textures, NULL, (void **)&pixBuffer, &pitch)) {
 
 			for (int y = 0; y < halfHeight; y++) {
 				for (int x = 0; x < width; x++) {
-
 					_fs = (camB / width) * (x - width / 2);
 					_sax = camX + 1.f / y * (halfHeight * cosf(camA + _fs)) / cosf(_fs);
 					_say = camY + 1.f / y * (halfHeight * sinf(camA + _fs)) / cosf(_fs);
 
 					if (_say < mapHeight && _say > 0 && _sax < mapWidth && _sax > 0) {
 						int ind = (int)_say * mapWidth + (int)_sax;
-
 						mapFloor = flatFloor[ind];
 						ceil_tile = flatCiel[ind];
 						_sax = _sax - (int)_sax;
 						_say = _say - (int)_say;
-
 						int _cpx = (ceil_tile % mapWallWw) * mapWallW + _sax * mapWallW, _cpy = (ceil_tile / mapWallWw) * mapWallH + _say * mapWallH;
 						int _fpx = (mapFloor % mapWallW) * mapWallW + _sax * mapWallW, _fpy = (mapFloor / mapWallWw) * mapWallH + _say * mapWallH;
-						
 						ceilColor = getpixel(wallSurf, _cpx, _cpy);
-
-						if (ceilColor != colorConv) { // ceiling
+						
+						/* ceiling */
+						if (ceilColor != colorConv) {
 							_fcr = RGBwallSurf[_cpx + _cpy * wallSurf->w][0];
 							_fcg = RGBwallSurf[_cpx + _cpy * wallSurf->w][1];
 							_fcb = RGBwallSurf[_cpx + _cpy * wallSurf->w][2];
 							pixBuffer[halfHeight * width - y * width + x - width] = RGB(_fcr * mapLight[ind], _fcg * mapLight[ind], _fcb * mapLight[ind]);
 						}
-						else {  // ciel
+
+						/* ciel */
+						else {
 							slice = (camA + _fs) / 3.14;
 							if (slice < 0) slice++;
 							if (slice > 1) slice--;
-
 							pixBuffer[halfHeight * width - y * width + x - width] = getpixel(cielSurf, slice * cielSurf->w, (float)cielSurf->h / (halfHeight) * (halfHeight - y));
 						}
 
-						// floor
+						/* floor */
 						_fcr = RGBwallSurf[_fpx + _fpy * wallSurf->w][0];
 						_fcg = RGBwallSurf[_fpx + _fpy * wallSurf->w][1];
 						_fcb = RGBwallSurf[_fpx + _fpy * wallSurf->w][2];
-
 						pixBuffer[y * width + x + halfHeight * width] = RGB(_fcr * mapLight[ind], _fcg * mapLight[ind], _fcb * mapLight[ind]);
 					}
 				}
 			}
-			for (int i = 0; i < renderDataSize; i++) {
-				if (renderData[i][0] == 0) { // walls
-					dop = 0;
 
+			for (int i = 0; i < renderDataSize; i++) {
+
+				/* walls */
+				if (renderData[i][0] == 0) {
+					dop = 0;
 					cheight = height / renderData[i][1] / cosf(renderData[i][3] - camA);
+
 					if (cheight > height) {
 						dop = (cheight - height) / 2;
 						cheight = height;
 					}
 
-					wallNumL = ((int)renderData[i][2] - 1) % mapWallWw; // wall image number from left border of the map
-					wallNumT = ((int)renderData[i][2] - 1) / mapWallWw; // wall image number from top border of the map
+					/* wall image number from left border of the map */
+					wallNumL = ((int)renderData[i][2] - 1) % mapWallWw; 
+					/* wall image number from top border of the map */
+					wallNumT = ((int)renderData[i][2] - 1) / mapWallWw; 
 
 					int sp = (halfHeight - cheight / 2) - dop, ep = (halfHeight + cheight / 2) + dop - 1;
 					int _cpx = wallNumL * mapWallW + (int)(mapWallW * renderData[i][5]), _cpy = 0;
@@ -397,21 +500,26 @@ public:
 						pixBuffer[(int)renderData[i][4] + j * width] = RGB(_fcr * renderData[i][6], _fcg * renderData[i][6], _fcb * renderData[i][6]);
 					}
 				}
-				if (renderData[i][0] == 1) { // Objs
+
+				/* Objs */
+				if (renderData[i][0] == 1) {
 					wallNumL = (int)renderData[i][2] % mapObjsWw;
 					wallNumT = renderData[i][2] / mapObjsWw;
-
 					int _cpx = wallNumL * mapObjsW, _cpy = wallNumT * mapObjsH;
 					int _sdy = (renderData[i][4] < 0 ? 0 : renderData[i][4]), _sdx = (renderData[i][3] < 0 ? 0 : renderData[i][3]),
 						_edy = (renderData[i][4] + renderData[i][5] >= height ? height : renderData[i][4] + renderData[i][5]),
 						_edx = (renderData[i][3] + renderData[i][5] >= width ? width : renderData[i][3] + renderData[i][5]);
 
-					int xdt = _sdx - renderData[i][3], ydt = _sdy - renderData[i][4]; // pixels out of the left bound
+					/* pixels out of the left bound */
+					int xdt = _sdx - renderData[i][3], ydt = _sdy - renderData[i][4];
 					int ind = 0;
+
 					for (int y = _sdy; y < _edy; y++) {
+
 						for (int x = _sdx; x < _edx; x++) {
 							xx = _cpx + (x - _sdx + xdt) * mapObjsW / renderData[i][5];
 							yy = _cpy + (y - _sdy + ydt) * mapObjsH / renderData[i][5];
+
 							if (getpixel(objSurf, xx, yy) != colorConv) {
 								ind = xx + yy * objSurf->w;
 								_fcr = RGBobjSurf[ind][0];
@@ -423,17 +531,29 @@ public:
 					}
 				}
 			}
+
+			/* Unlocked textures */
 			SDL_UnlockTexture(textures);
+			/* copied the rendered textures to display it on the screen.*/
 			SDL_RenderCopy(renderer, textures, NULL, NULL);
 		}
-		else
-			std::cout << "not locked\n";
-	}
 
+		else {
+			std::cout << "Error\n";
+		}
+	};
+
+	/**
+	 * close - Frees the memory used by the wall and object textures.
+	 */
 	void close() {
+
+		/* Free the wall texture */
 		SDL_DestroyTexture(walls);
+		/* Free the object texture */
 		SDL_DestroyTexture(Objs);
 	}
+
 };
 
 #endif /* _GAME_HPP_ */
--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,28 @@
 int main(int argc, char *args[])
 {
 
+    /* Player position and direccion */
 	float plaX = 1.5, plaY = 8.5, dirX = 0.0, dirY = 1.0;
+
 	bool run = true;
 	int time, oldTime = 0, frameTime = 0;
 	int mouseX, mouseY;
 	float posX, posY;
 
+    /* Initialize SDL*/
 	ASSERT(!SDL_Init(SDL_INIT_VIDEO),
 			"SDL failed to initialize; %s\n",
 			SDL_GetError());
 
-	/* create window */
+	/* Create window */
 	SDL_Window *window = SDL_CreateWindow("Game Maze", 100, 100, width, height, SDL_WINDOW_SHOWN|SDL_WINDOW_FULLSCREEN);
 	ASSERT(window, "failed to create window\n", SDL_GetError());
 
-	/* create renderer window */
+	/* Create renderer window */
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	ASSERT(renderer, "failed to create renderer\n", SDL_GetError());
 
+    /* Create game objets */
 	Game maze;
 	maze.setRenderer(renderer, width, height);
 	maze.setMapWalls("pics/walls.bmp", 32, 32);
@@ -44,21 +48,18 @@ int main(int argc, char *args[])
 	SDL_Event event;
 	SDL_ShowCursor(SDL_DISABLE);
 
-	while (run)
-	{
-
+	while (run) {
 		maze.show();
 		frameTime = 0;
 
-		/* timing for input and FPS counter */
-		while (frameTime < 1)
-		{
+		/* Timing for input and FPS counter */
+		while (frameTime < 1) {
 			time = SDL_GetTicks();
 			frameTime += time - oldTime;
 			oldTime = time;
 		}
 
-		/* frametime is the time this frame has taken, in seconds */
+		/* Frametime is the time this frame has taken, in seconds */
 		frameTime = 1000 / frameTime;
 
 		posX = 0;
@@ -66,40 +67,35 @@ int main(int argc, char *args[])
 		const Uint8* keys = SDL_GetKeyboardState(NULL);
 		float moveSpeed = 0.05;
 
-		/* move forward */
-		if (keys[SDL_SCANCODE_W])
-		{
+		/* Move forward */
+		if (keys[SDL_SCANCODE_W]) {
 			posX += moveSpeed * cosf(dirX) * 60.f / frameTime;
 			posY += moveSpeed * sinf(dirX) * 60.f / frameTime;
 		}
 
-		/* move left */
-		if (keys[SDL_SCANCODE_A])
-		{
+		/* Move left */
+		if (keys[SDL_SCANCODE_A]) {
 			posX += moveSpeed * cosf(dirX - 1.57) * 60.f / frameTime;
 			posY += moveSpeed * sinf(dirX - 1.57) * 60.f / frameTime;
 		}
 
-		/* move right */
-		if (keys[SDL_SCANCODE_D])
-		{
+		/* Move right */
+		if (keys[SDL_SCANCODE_D]) {
 			posX += moveSpeed * cosf(dirX + 1.57) * 60.f / frameTime;
 			posY += moveSpeed * sinf(dirX + 1.57) * 60.f / frameTime;
 		}
 
-		/* move back */
-		if (keys[SDL_SCANCODE_S])
-		{
+		/* Move back */
+		if (keys[SDL_SCANCODE_S]) {
 			posX -= moveSpeed * cosf(dirX) * 60.f / frameTime;
 			posY -= moveSpeed * sinf(dirX) * 60.f / frameTime;
 		}
 
-		if \
-		(!map[(int)(plaY + posY + .3) * MAPWIDTH + (int)(plaX + posX + .3)] &&
+        /* Check for collicion with walls */
+		if (!map[(int)(plaY + posY + .3) * MAPWIDTH + (int)(plaX + posX + .3)] &&
 		!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX + posX + .3)] &&
 		!map[(int)(plaY + posY + .3) * MAPWIDTH + (int)(plaX + posX - .3)] &&
-		!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX + posX - .3)])
-		{
+		!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX + posX - .3)]) {
 			plaX += posX;
 			plaY += posY;
 		}
@@ -107,43 +103,47 @@ int main(int argc, char *args[])
 		if (!map[(int)(plaY + posY + .3) * MAPWIDTH + (int)(plaX + .3)] &&
 			!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX + .3)] &&
 			!map[(int)(plaY + posY + .3) * MAPWIDTH + (int)(plaX - .3)] &&
-			!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX - .3)])
-			{
+			!map[(int)(plaY + posY - .3) * MAPWIDTH + (int)(plaX - .3)]) {
 				plaY += posY;
 		}
 
 		if (!map[(int)(plaY + .3) * MAPWIDTH + (int)(plaX + posX + .3)] &&
 			!map[(int)(plaY - .3) * MAPWIDTH + (int)(plaX + posX + .3)] &&
 			!map[(int)(plaY + .3) * MAPWIDTH + (int)(plaX + posX - .3)] &&
-			!map[(int)(plaY - .3) * MAPWIDTH + (int)(plaX + posX - .3)])
-			{
+			!map[(int)(plaY - .3) * MAPWIDTH + (int)(plaX + posX - .3)]) {
 				plaX += posX;
 		}
 
-		/* turn the camera to left */
-		if (keys[SDL_SCANCODE_LEFT])
-			dirX -= moveSpeed * 60.f / frameTime / 2;
+		/* Turn the camera to left */
+		if (keys[SDL_SCANCODE_LEFT]) {
+            dirX -= moveSpeed * 60.f / frameTime / 2;
+        }
 
-		/* turn the camera to right */
-		if (keys[SDL_SCANCODE_RIGHT])
-			dirX += moveSpeed * 60.f / frameTime / 2;
+		/* Turn the camera to right */
+		if (keys[SDL_SCANCODE_RIGHT]) {
+            dirX += moveSpeed * 60.f / frameTime / 2;
+        }
 
-		/* event loop */
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-				run = false;
-			if (event.type == SDL_KEYDOWN)
-			{
-				if (event.key.keysym.sym == SDLK_ESCAPE)
-					run = false;
+		/* Event loop */
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+                run = false;
+            }
+
+            /* Checks if a key was pressed */
+			if (event.type == SDL_KEYDOWN) {
+
+                /* Checks if the Escape key was pressed */
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    run = false;
+                }
 			}
 		}
 
 		/* Retrieve the current state of the mouse. */
 		SDL_GetMouseState(&mouseX, &mouseY);
 
-		/* mouse rotation velocity */
+		/* Mouse rotation velocity */
 		dirX += (mouseX - width / 2) * 0.00070;
 
 		/* Move the mouse to the given position in global screen space. */
@@ -152,7 +152,7 @@ int main(int argc, char *args[])
 		/* Updates the screen with any rendering made in a previous call. */
 		SDL_RenderPresent(renderer);
 
-		/* set cam position */
+		/* Set camera position */
 		maze.setCam(plaX, plaY, dirX, dirY);
 	}
 
